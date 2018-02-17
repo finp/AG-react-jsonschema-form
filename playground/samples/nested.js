71699f690a61e95a4f270a63564a07982ef2b8ad
Paul Wright <pwright@redhat.com>
2018-02-17 11:16:31 +0000

    mobile.next

--- playground/samples/nested.js
+++ playground/samples/nested.js
@@ -1,16 +1,15 @@
 module.exports = {
   schema: {
-    title: "A list of tasks",
+    title: "AeroGear Mobile.next",
     type: "object",
-    required: ["title"],
     properties: {
       title: {
         type: "string",
-        title: "Task list title",
+        title: "AeroGear",
       },
-      tasks: {
+      services: {
         type: "array",
-        title: "Tasks",
+        title: "Services",
         items: {
           type: "object",
           required: ["title"],
@@ -18,46 +17,230 @@ module.exports = {
             title: {
               type: "string",
               title: "Title",
-              description: "A sample title",
+              description: "Service",
             },
-            details: {
+            repo: {
               type: "string",
-              title: "Task details",
-              description: "Enter the task details",
+              title: "repo",
+              description: "Enter the repo details",
             },
-            done: {
+            archived: {
               type: "boolean",
-              title: "Done?",
+              title: "Archived",
               default: false,
+            },
+            versions: {
+              type: "array",
+              title: "Versions",
+              items: {
+                type: "object",
+                required: ["version"],
+                properties: {
+                  version: {
+                    type: "string",
+                    title: "Version",
+                    description: "Version",
+                  },
+                  tag: {
+                    type: "string",
+                    title: "tag",
+                    description: "Enter the tag details",
+                  },
+                  archived: {
+                    type: "boolean",
+                    title: "Archived",
+                    default: false,
+                  },
+                  compatibleVersions: {
+                    type: "array",
+                    title: "compatibleVersions",
+                    items: {
+                      type: "object",
+                      properties: {
+                        Component: {
+                          type: "string",
+                          title: "Component",
+                          description: "Component",
+                        },
+                        version: {
+                          type: "string",
+                          title: "Version",
+                          description: "Component Version",
+                        },
+                        tag: {
+                          type: "string",
+                          title: "tag",
+                          description: "Enter the tag details",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      sdk: {
+        type: "array",
+        title: "SDK",
+        items: {
+          type: "object",
+          required: ["title"],
+          properties: {
+            title: {
+              type: "string",
+              title: "Title",
+              description: "SDK",
+            },
+            repo: {
+              type: "string",
+              title: "repo",
+              description: "Enter the repo details",
+            },
+            archived: {
+              type: "boolean",
+              title: "Archived",
+              default: false,
+            },
+            versions: {
+              type: "array",
+              title: "Versions",
+              items: {
+                type: "object",
+                required: ["title"],
+                properties: {
+                  title: {
+                    type: "string",
+                    title: "Title",
+                    description: "Version",
+                  },
+                  tag: {
+                    type: "string",
+                    title: "tag",
+                    description: "Enter the tag details",
+                  },
+                  archived: {
+                    type: "boolean",
+                    title: "Archived",
+                    default: false,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      other: {
+        type: "array",
+        title: "Other",
+        items: {
+          type: "object",
+          required: ["title"],
+          properties: {
+            title: {
+              type: "string",
+              title: "Title",
+              description: "Other",
+            },
+            repo: {
+              type: "string",
+              title: "repo",
+              description: "Enter the repo details",
+            },
+            archived: {
+              type: "boolean",
+              title: "Archived",
+              default: false,
+            },
+            versions: {
+              type: "array",
+              title: "Versions",
+              items: {
+                type: "object",
+                required: ["title"],
+                properties: {
+                  title: {
+                    type: "string",
+                    title: "Title",
+                    description: "Version",
+                  },
+                  tag: {
+                    type: "string",
+                    title: "tag",
+                    description: "Enter the tag details",
+                  },
+                  archived: {
+                    type: "boolean",
+                    title: "Archived",
+                    default: false,
+                  },
+                },
+              },
             },
           },
         },
       },
     },
   },
-  uiSchema: {
-    tasks: {
-      items: {
-        details: {
-          "ui:widget": "textarea",
-        },
-      },
-    },
-  },
+  uiSchema: {},
   formData: {
-    title: "My current tasks",
-    tasks: [
+    title: "Mobile.next",
+    services: [
       {
-        title: "My first task",
-        details:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        done: true,
+        title: "Sync",
+        archived: false,
+        versions: [
+          {
+            version: "V1.0",
+            tag: "v1.0.4",
+            archived: false,
+          },
+          {
+            version: "V1.1",
+            tag: "v1.1.4",
+            archived: false,
+          },
+        ],
       },
       {
-        title: "My second task",
-        details:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-        done: false,
+        title: "Push",
+        archived: false,
+        versions: [
+          {
+            version: "V2.0",
+            tag: "v2.0.0",
+            archived: false,
+          },
+        ],
+      },
+    ],
+    sdk: [
+      {
+        title: "Android",
+        archived: false,
+      },
+      {
+        title: "iOS",
+        archived: false,
+      },
+      {
+        title: "Cordova",
+        archived: false,
+      },
+    ],
+    other: [
+      {
+        title: "Mobile CLI",
+        repo: "https://github.com/aerogear/mobile-cli",
+        archived: false,
+        versions: [
+          {
+            title: "Preview",
+            tag: "v0.0.4",
+            archived: false,
+          },
+        ],
       },
     ],
   },
